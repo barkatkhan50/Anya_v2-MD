c3ec1b7a9f97612a0036dad6604cab59d6075a57
barkatkhan50 <103475269+barkatkhan50@users.noreply.github.com>
2024-10-15 18:25:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
      * owner details and parameters
      */
     ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownernumber: process.env.Owner_Number || "923052199342",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "ZHpiOGt_Queen-Anya_lM2E=",
 
     /**
      * thumbnails and medias
